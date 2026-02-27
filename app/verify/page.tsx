69abd2c909ede25056a7c219d3bde37f114a4ef3
janogale <mukhtar.mahomed@gmail.com>
2026-02-27 14:32:35 +0300

    change a tag to button

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -6,7 +6,7 @@ export default function VerificationPage() {
      const params = useSearchParams();
   return (
     <div className="min-h-screen bg-gray-200 flex items-center justify-center">
-      <div className="bg-[#4189dd]   text-center p-14">
+      <div className="bg-[#4189dd]   text-center p-14 flex flex-col items-center justify-center">
         
         <h1 className="text-black text-2xl  font-serif font-semibold leading-snug">
           You will require your Passport Number to open the
@@ -14,12 +14,14 @@ export default function VerificationPage() {
           Verification PDF
         </h1>
 
-        <a
-          href={`verify/${params.get("etas") ?? "1"}`}
-          className="block text-2xl text-purple-900 font-serif font-bold underline"
+        <button
+          onClick={() => {
+            window.location.href = `verify/${params.get("etas") ?? "1"}`;
+          }}
+          className="block text-2xl text-purple-900 font-serif font-bold underline text-center"
         >
           CLICK HERE TO PROCEED
-        </a>
+        </button>
 
       </div>
     </div>
